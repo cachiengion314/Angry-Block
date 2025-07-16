8cbeac33b8b4b2073dee4652ce844288f78971ae
NguyenTanNguyen <nguyentannguyen002@gmail.com>
2025-07-16 15:03:09 +0700

    fix logic booster 2

--- Assets/Game/Scripts/Managers/LevelManager/LogicBoosterManager.cs
+++ Assets/Game/Scripts/Managers/LevelManager/LogicBoosterManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using Unity.Mathematics;
 using UnityEngine;
@@ -6,81 +7,94 @@ using UnityEngine.Pool;
 
 public partial class LevelManager
 {
-    public bool IsTriggerBooster1 = false;
-    public void OnTriggerBooster1(GameObject directionBlock)
-    {
-        if (directionBlock == null) return;
-        var emptyWaitingSlot = FindEmptySlotFrom(_waitingSlots);
-        if (emptyWaitingSlot == -1 || emptyWaitingSlot > _waitingSlots.Length - 1) return;
+  public bool IsTriggerBooster1 = false;
+  public void OnTriggerBooster1(GameObject directionBlock)
+  {
+    if (directionBlock == null) return;
+    var emptyWaitingSlot = FindEmptySlotFrom(_waitingSlots);
+    if (emptyWaitingSlot == -1 || emptyWaitingSlot > _waitingSlots.Length - 1) return;
 
-        IsTriggerBooster1 = false;
-        GameManager.Instance.Booster1--;
+    IsTriggerBooster1 = false;
+    GameManager.Instance.Booster1--;
 
-        MoveToSlots(emptyWaitingSlot, directionBlock, _waitingSlots, _waitingPositions);
-    }
-    void MoveToSlots(
+    MoveToSlots(emptyWaitingSlot, directionBlock, _waitingSlots, _waitingPositions);
+  }
+
+  void MoveToSlots(
     int slotIndex,
     GameObject directionBlock,
     GameObject[] slots,
     in float3[] positions
   )
+  {
+    if (!directionBlock.TryGetComponent<IMoveable>(out var moveable)) return;
+    if (slotIndex > positions.Length - 1 || slotIndex < 0)
     {
-        if (!directionBlock.TryGetComponent<IMoveable>(out var moveable)) return;
-        if (slotIndex > positions.Length - 1 || slotIndex < 0)
-        {
-            print("Slots is not available");
-            return;
-        }
-        var duration = 0.3f;
-        var endPos = positions[slotIndex];
-
-        // logic
-        if (!directionBlock.TryGetComponent(out IColorBlock colorBlock)) return;
-        _directionBlocks[colorBlock.GetIndex()] = null;
-        var index = bottomGrid.ConvertWorldPosToIndex(endPos);
-        colorBlock.SetIndex(index);
-        _directionBlocks[index] = directionBlock;
-        slots[slotIndex] = directionBlock;
-
-        moveable.SetLockedPosition(endPos);
-        directionBlock.transform.DOMove(endPos, duration)
-        .SetEase(Ease.Linear)
-        .OnComplete(() => moveable.SetLockedPosition(0));
+      print("Slots is not available");
+      return;
     }
+    var duration = 0.3f;
+    var endPos = positions[slotIndex];
 
-    public void OnTriggerBooster2()
+    // logic
+    if (!directionBlock.TryGetComponent(out IColorBlock colorBlock)) return;
+    _directionBlocks[colorBlock.GetIndex()] = null;
+    var index = bottomGrid.ConvertWorldPosToIndex(endPos);
+    colorBlock.SetIndex(index);
+    _directionBlocks[index] = directionBlock;
+    slots[slotIndex] = directionBlock;
+
+    moveable.SetLockedPosition(endPos);
+    directionBlock.transform.DOMove(endPos, duration)
+    .SetEase(Ease.Linear)
+    .OnComplete(() => moveable.SetLockedPosition(0));
+  }
+
+  public void OnTriggerBooster2()
+  {
+    var directionBlockAvailables = DirectionBlocksAvailable();
+    for (int i = directionBlockAvailables.Length - 1; i >= 0; i--)
     {
-        var newDirectionBlocks = FindsDirectionBlocksNotNullAt(_directionBlocks);
-        for (int i = newDirectionBlocks.Length - 1; i >= 0; i--)
-        {
-            int j = UnityEngine.Random.Range(0, i + 1);
-            SwapAt(newDirectionBlocks[i],newDirectionBlocks[j]);
-        }
+      int j = UnityEngine.Random.Range(0, i + 1);
+      SwapAt(directionBlockAvailables[i], directionBlockAvailables[j]);
     }
+  }
 
-    GameObject[] FindsDirectionBlocksNotNullAt(GameObject[] directionBlocks)
+  GameObject[] DirectionBlocksAvailable()
+  {
+    var setDirectionBlocks = new HashSet<GameObject>(FindDirectionBlocksNotNullAt(_directionBlocks));
+    var setWaitingSlots = new HashSet<GameObject>(FindDirectionBlocksNotNullAt(_waitingSlots));
+    var setFiringSlots = new HashSet<GameObject>(FindDirectionBlocksNotNullAt(_firingSlots));
+
+    setDirectionBlocks.ExceptWith(setWaitingSlots);
+    setDirectionBlocks.ExceptWith(setFiringSlots);
+
+    return setDirectionBlocks.ToArray();
+  }
+
+  GameObject[] FindDirectionBlocksNotNullAt(GameObject[] directionBlocks)
+  {
+    List<GameObject> listDirectionBlock = new();
+    foreach (var directionBlock in directionBlocks)
     {
-        List<GameObject> listDirectionBlock = new();
-        foreach (var directionBlock in directionBlocks)
-        {
-            if (directionBlock == null) continue;
-            listDirectionBlock.Add(directionBlock);
-        }
-        return listDirectionBlock.ToArray();
+      if (directionBlock == null) continue;
+      listDirectionBlock.Add(directionBlock);
     }
+    return listDirectionBlock.ToArray();
+  }
 
-    void SwapAt(GameObject directionBlocks1, GameObject directionBlocks2)
-    {
-        if (!directionBlocks1.TryGetComponent(out DirectionBlockControl directionBlockControl1)) return;
-        if (!directionBlocks2.TryGetComponent(out DirectionBlockControl directionBlockControl2)) return;
+  void SwapAt(GameObject directionBlocks1, GameObject directionBlocks2)
+  {
+    if (!directionBlocks1.TryGetComponent(out DirectionBlockControl directionBlockControl1)) return;
+    if (!directionBlocks2.TryGetComponent(out DirectionBlockControl directionBlockControl2)) return;
 
-        var colorTemp = directionBlockControl1.GetColorValue();
-        var ammunitionTemp = directionBlockControl1.GetAmmunition();
+    var colorTemp = directionBlockControl1.GetColorValue();
+    var ammunitionTemp = directionBlockControl1.GetAmmunition();
 
-        directionBlockControl1.SetColorValue(directionBlockControl2.GetColorValue());
-        directionBlockControl1.SetAmmunition(directionBlockControl2.GetAmmunition());
+    directionBlockControl1.SetColorValue(directionBlockControl2.GetColorValue());
+    directionBlockControl1.SetAmmunition(directionBlockControl2.GetAmmunition());
 
-        directionBlockControl2.SetColorValue(colorTemp);
-        directionBlockControl2.SetAmmunition(ammunitionTemp);
-    }
+    directionBlockControl2.SetColorValue(colorTemp);
+    directionBlockControl2.SetAmmunition(ammunitionTemp);
+  }
 }
